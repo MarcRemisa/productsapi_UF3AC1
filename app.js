1dbf0e4c97628083f040b6f430b74a117af46a6a
MarcRemisa <marc10remisa@gmail.com>
2024-04-30 16:40:02 +0200

    segundo commit

--- app.js
+++ app.js
@@ -1,21 +1,26 @@
-const express = require('express')
-const products_routes = require('./routes/products.js')
+const express = require('express');
+const slugify = require('slugify');
+const products_routes = require('./routes/products.js');
 
-//Server instantiation
-const app = express()
+// Server instantiation
+const app = express();
+const PORT = process.env.PORT || 5000;
 
-//Server configuration: template engine
+// Server configuration: template engine
 app.set('views', './views');
 app.set('view engine', 'pug');
 app.use(express.static('/views'));
 
-//Midleware
-app.use(express.json())
-app.use('/', products_routes)
+// Middleware
+app.use(express.json());
+app.use('/', products_routes);
 
-//Server startup
-app.listen(5000, () => {
-    console.log('server is listening on port 5000')
-})
+// Slugify welcome message
+const welcomeMessage = "Welcome to our Express server!";
+const slugifiedWelcomeMessage = slugify(welcomeMessage, { replacement: '*', lower: true });
 
+console.log(`Slugified welcome message: ${slugifiedWelcomeMessage}`);
 
+app.listen(PORT, () => {
+  console.log(`Server started on port ${PORT}`);
+});
