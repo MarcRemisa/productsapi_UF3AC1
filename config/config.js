dc77b7b8d25abf0a1f892b8e3213a7aa6bafc918
MarcRemisa <marc10remisa@gmail.com>
2024-04-30 17:37:27 +0200

    commit final

--- config/config.js
+++ config/config.js
@@ -1,3 +1,5 @@
-module.exports = {
-  mongoURI: "mongodb+srv://marc10remisa:LXwAzJIW9TaA0LTT@products.eazfuov.mongodb.net/?retryWrites=true&w=majority&appName=products"
+const config = {
+  mongoURI: process.env.MONGOURI
 };
+
+module.exports = config;
